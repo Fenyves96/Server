49511e8dce75f394a980a3f4f835676b48b44526
Fenyves96 <fenyves96@hotmail.com>
2018-03-04 14:21:36 +0100

    Update1

---
+++ SocketServer/CommObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Communication
+{
+    [Serializable]
+    public class CommObject
+    {
+        public string Sender { get; set; }
+        public string Message { get; set; }
+        public DateTime Date { get; set; }
+
+        public CommObject() { }
+        public CommObject(string sender,string msg)
+        {
+            this.Sender = sender;
+            this.Message = msg;
+            this.Date = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return Sender + " " + Message;
+          
+        }
+    }
+}
